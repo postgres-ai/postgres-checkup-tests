8321ce687e95c163df8e77fde1b5dfcc0a2625fc
postgres-ai <nik@postgres.ai>
2019-02-27 13:29:47 +0000

    issue_237 artifacts

--- issue_237/json_reports/1_2019_02_27T12_43_11_+0000/K_query_groups/14.sql
+++
@@ -1,6 +0,0 @@
--- queryid: -4605761145085266400
-select
- json_build_object($1, version(),
- $2, current_setting($3),
- $4, (select regexp_replace(current_setting($5), $6, $7)),
- $8, (select regexp_replace(current_setting($9), $10, $11)))
