c696500cf32241b926e9fe12a48b0a419f07234a
postgres-ai <nik@postgres.ai>
2019-03-19 13:19:25 +0000

    dmus-h002-fix artifacts

---
+++ dmus-h002-fix/json_reports/1_2019_03_19T13_18_14_+0000/K_query_groups/19.sql
@@ -0,0 +1,4 @@
+-- queryid: 5288109407298079000
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '42'
+create index concurrently i_94 on t_94(i)
---
+++ dmus-h002-fix/json_reports/1_2019_03_19T13_18_14_+0000/K_query_groups/59.sql
@@ -0,0 +1,4 @@
+-- queryid: 5369673549656116000
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '29'
+select count(*) from t_slw_q
