6bd870a88c7d5e5d78c4f3732d6b21515ae1252c
postgres-ai <nik@postgres.ai>
2019-04-11 19:28:21 +0000

    348-dmius-md2pdf artifacts

---
+++ 348-dmius-md2pdf/json_reports/1_2019_04_11T19_27_14_+0000/K_query_groups/61_1.sql
@@ -0,0 +1,4 @@
+-- queryid: 22211663676525936
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '109'
+create table t_fk_2 as select id, (random() * 1000000)::int8 as t1_id from generate_series(1, 1000000) _(id)
