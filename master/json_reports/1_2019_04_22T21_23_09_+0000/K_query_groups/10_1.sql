55073cb2c7f7253b716a9e686320b0bf1d640848
postgres-ai <nik@postgres.ai>
2019-04-22 21:49:28 +0000

    master artifacts

--- master/json_reports/1_2019_04_22T21_23_09_+0000/K_query_groups/10_1.sql
+++
@@ -1,9 +0,0 @@
--- queryid: -3798127187466836000
--- NOTICE: the first 50k characters
--- NOTICE: current query size (bytes): '325'
-with stat_statements as (
- select json_object_agg(pg_settings.name, pg_settings) as json from pg_settings where name ~ $1
-), kcache as (
- select json_object_agg(pg_settings.name, pg_settings) as json from pg_settings where name ~ $2
-)
-select json_build_object($3, (select * from stat_statements), $4, (select * from kcache))
