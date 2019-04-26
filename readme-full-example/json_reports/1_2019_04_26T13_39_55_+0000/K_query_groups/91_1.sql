e66bc94d90316bab18df2a0014d1fdc73f8e0d67
postgres-ai <nik@postgres.ai>
2019-04-26 13:41:05 +0000

    readme-full-example artifacts

---
+++ readme-full-example/json_reports/1_2019_04_26T13_39_55_+0000/K_query_groups/91_1.sql
@@ -0,0 +1,4 @@
+-- queryid: -3115028882787390000
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '91'
+create index t_with_redundant_ref_idx_2 on public.t_with_redundant_ref_idx using btree (i)
