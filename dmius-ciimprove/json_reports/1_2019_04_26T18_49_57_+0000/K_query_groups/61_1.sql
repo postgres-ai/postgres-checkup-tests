0add7cfbd4afc4fe12474526551f1882dc547a99
postgres-ai <nik@postgres.ai>
2019-04-26 19:08:35 +0000

    dmius-ciimprove artifacts

--- dmius-ciimprove/json_reports/1_2019_04_26T18_49_57_+0000/K_query_groups/61_1.sql
+++
@@ -1,4 +0,0 @@
--- queryid: 8565802378619238000
--- NOTICE: the first 50k characters
--- NOTICE: current query size (bytes): '80'
-create index concurrently t_with_redundant_idx_f3_2 on t_with_redundant_idx(f3)
--- dmius-ciimprove/json_reports/1_2019_04_26T18_49_57_+0000/K_query_groups/80_1.sql
+++
@@ -1,4 +0,0 @@
--- queryid: -94268938950487040
--- NOTICE: the first 50k characters
--- NOTICE: current query size (bytes): '98'
-create index t_with_redundant_ref_idx_2 on exp_redundant.t_with_redundant_ref_idx using btree (i)
