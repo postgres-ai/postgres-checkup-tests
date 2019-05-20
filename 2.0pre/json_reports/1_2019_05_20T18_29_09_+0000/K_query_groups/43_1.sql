d4f8efb9b8a3628bb7a03d2273acddbab2c31c65
postgres-ai <nik@postgres.ai>
2019-05-20 18:30:15 +0000

    2.0pre artifacts

---
+++ 2.0pre/json_reports/1_2019_05_20T18_29_09_+0000/K_query_groups/43_1.sql
@@ -0,0 +1,4 @@
+-- queryid: 879148290678321700
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '80'
+create index concurrently t_with_redundant_idx_f1_2 on t_with_redundant_idx(f1)
