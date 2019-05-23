267727d8f87e583a0e2c537513cb36320d87b862
postgres-ai <nik@postgres.ai>
2019-05-23 10:06:23 +0000

    revert-cac4b67a artifacts

---
+++ revert-cac4b67a/json_reports/1_2019_05_23T10_05_14_+0000/K_query_groups/15_1.sql
@@ -0,0 +1,4 @@
+-- queryid: -1845904424067371800
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '98'
+create index t_with_redundant_ref_idx_3 on exp_redundant.t_with_redundant_ref_idx using btree (i)
