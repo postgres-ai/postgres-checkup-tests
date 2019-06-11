3b30f0918f92ea7ea43496eedf39dd4da442c4d3
postgres-ai <nik@postgres.ai>
2019-06-11 09:04:42 +0000

    395-dmius-resports-struct artifacts

--- 395-dmius-resports-struct/json_reports/1_2019_06_11T08_10_55_+0000/K_query_groups/45_1.sql
+++
@@ -1,4 +0,0 @@
--- queryid: 7628260016404083000
--- NOTICE: the first 50k characters
--- NOTICE: current query size (bytes): '42'
-create index concurrently i_73 on t_73(i)
--- 395-dmius-resports-struct/json_reports/1_2019_06_11T08_10_55_+0000/K_query_groups/72_1.sql
+++
@@ -1,4 +0,0 @@
--- queryid: 8296847210748937000
--- NOTICE: the first 50k characters
--- NOTICE: current query size (bytes): '42'
-create index concurrently i_86 on t_86(i)
