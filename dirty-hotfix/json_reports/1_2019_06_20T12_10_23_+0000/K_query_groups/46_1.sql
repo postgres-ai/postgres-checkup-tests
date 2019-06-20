2857218b6f7f4c68d21dd9434dcc3ec5732855b5
postgres-ai <nik@postgres.ai>
2019-06-20 12:11:19 +0000

    dirty-hotfix artifacts

---
+++ dirty-hotfix/json_reports/1_2019_06_20T12_10_23_+0000/K_query_groups/46_1.sql
@@ -0,0 +1,4 @@
+-- queryid: 3807976279930832000
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '29'
+select count(*) from t_slw_q
---
+++ dirty-hotfix/json_reports/1_2019_06_20T12_10_23_+0000/K_query_groups/63_1.sql
@@ -0,0 +1,4 @@
+-- queryid: 5631462763178648000
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '42'
+create index concurrently i_49 on t_49(i)
