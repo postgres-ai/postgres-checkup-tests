8137e915cb8d5e0adcb608baf907356b891a92d1
postgres-ai <nik@postgres.ai>
2019-06-27 14:09:50 +0000

    master artifacts

---
+++ master/json_reports/1_2019_06_27T14_08_51_+0000/K_query_groups/76_1.sql
@@ -0,0 +1,4 @@
+-- queryid: -2801630118753177600
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '54'
+create index r_red_fk_2_fk_idx on t_red_fk_2(r_t1_id)
---
+++ master/json_reports/1_2019_06_27T14_08_51_+0000/K_query_groups/77_1.sql
@@ -0,0 +1,4 @@
+-- queryid: 7121640066867289000
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '91'
+create index t_with_redundant_ref_idx_1 on public.t_with_redundant_ref_idx using btree (i)
