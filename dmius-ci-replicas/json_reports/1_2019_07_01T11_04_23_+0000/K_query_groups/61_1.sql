c8222900587e5bee3d9d6b2097f61da8ce45e55e
postgres-ai <nik@postgres.ai>
2019-07-01 17:00:40 +0000

    dmius-ci-replicas artifacts

--- dmius-ci-replicas/json_reports/1_2019_07_01T11_04_23_+0000/K_query_groups/61_1.sql
+++
@@ -1,4 +0,0 @@
--- queryid: -8165656514688398000
--- NOTICE: the first 50k characters
--- NOTICE: current query size (bytes): '42'
-create index concurrently i_69 on t_69(i)
--- dmius-ci-replicas/json_reports/1_2019_07_01T11_04_23_+0000/K_query_groups/73_1.sql
+++
@@ -1,4 +0,0 @@
--- queryid: 4326020655759962600
--- NOTICE: the first 50k characters
--- NOTICE: current query size (bytes): '65'
-create table t_63 as select i from generate_series(1, 1000) _(i)
