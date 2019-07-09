0afd3ba29ba8d452c4146815dc157b935711f653
postgres-ai <nik@postgres.ai>
2019-07-09 19:32:22 +0000

    dmius-a001-uname-o-fix artifacts

--- dmius-a001-uname-o-fix/json_reports/1_2019_07_09T19_28_59_+0000/K_query_groups/75_3.sql
+++
@@ -1,4 +0,0 @@
--- queryid: -949153349382420900
--- NOTICE: the first 50k characters
--- NOTICE: current query size (bytes): '42'
-create index concurrently i_76 on t_76(i)
---
+++ dmius-a001-uname-o-fix/json_reports/1_2019_07_09T19_30_32_+0000/K_query_groups/5_3.sql
@@ -0,0 +1,4 @@
+-- queryid: 6127281624844695000
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '64'
+create table t_9 as select i from generate_series(1, 1000) _(i)
