95b45b1ed396da2b5de7a9ff490a51945dbe3892
postgres-ai <nik@postgres.ai>
2019-07-22 08:24:27 +0000

    master artifacts

---
+++ master/json_reports/1_2019_07_22T08_22_53_+0000/K_query_groups/55_3.sql
@@ -0,0 +1,4 @@
+-- queryid: -5638444402614152000
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '41'
+create index concurrently i_15 on t_15(i)
---
+++ master/json_reports/1_2019_07_22T08_22_53_+0000/K_query_groups/69_3.sql
@@ -0,0 +1,4 @@
+-- queryid: 3421899641166660600
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '41'
+create index concurrently i_48 on t_48(i)
