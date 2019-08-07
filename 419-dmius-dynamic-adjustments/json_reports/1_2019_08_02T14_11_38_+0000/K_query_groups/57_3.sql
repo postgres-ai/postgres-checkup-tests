d0ec1b6a3229a0ec414b7208b6fe74d41158edf2
postgres-ai <nik@postgres.ai>
2019-08-07 09:02:03 +0000

    419-dmius-dynamic-adjustments artifacts

--- 419-dmius-dynamic-adjustments/json_reports/1_2019_08_02T14_11_38_+0000/K_query_groups/57_3.sql
+++
@@ -1,4 +0,0 @@
--- queryid: 8722841963539232000
--- NOTICE: the first 50k characters
--- NOTICE: current query size (bytes): '43'
-create index concurrently i_r_10 on t_10(i)
--- 419-dmius-dynamic-adjustments/json_reports/1_2019_08_02T14_11_38_+0000/K_query_groups/80_3.sql
+++
@@ -1,4 +0,0 @@
--- queryid: 1268935802788983600
--- NOTICE: the first 50k characters
--- NOTICE: current query size (bytes): '43'
-create index concurrently i_r_23 on t_23(i)
