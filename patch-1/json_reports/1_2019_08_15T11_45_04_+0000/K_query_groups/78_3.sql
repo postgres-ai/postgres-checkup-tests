991d841eecf903dd27282c0f538d5ddee58c7034
postgres-ai <nik@postgres.ai>
2019-08-15 12:22:24 +0000

    patch-1 artifacts

--- patch-1/json_reports/1_2019_08_15T11_45_04_+0000/K_query_groups/78_3.sql
+++
@@ -1,4 +0,0 @@
--- queryid: -3717551161508284400
--- NOTICE: the first 50k characters
--- NOTICE: current query size (bytes): '41'
-create index concurrently i_r_1 on t_1(i)
---
+++ patch-1/json_reports/1_2019_08_15T12_20_39_+0000/K_query_groups/19_3.sql
@@ -0,0 +1,4 @@
+-- queryid: 5622946096907204000
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '43'
+create index concurrently i_r_43 on t_43(i)
