9e12a27061dabf1d3b802f7c94168e7b40f28e67
postgres-ai <nik@postgres.ai>
2019-08-16 14:54:50 +0000

    dmius-l003-fix artifacts

--- dmius-l003-fix/json_reports/1_2019_08_12T17_18_21_+0000/K_query_groups/16_3.sql
+++
@@ -1,4 +0,0 @@
--- queryid: -5169817456611317000
--- NOTICE: the first 50k characters
--- NOTICE: current query size (bytes): '49'
-create extension if not exists pg_stat_statements
---
+++ dmius-l003-fix/json_reports/1_2019_08_16T14_53_01_+0000/K_query_groups/26_3.sql
@@ -0,0 +1,4 @@
+-- queryid: 1355625432779853600
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '43'
+create index concurrently i_u_56 on t_56(i)
