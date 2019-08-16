d0012bace94cf720c96036d55f8d4924b4e2f5f3
postgres-ai <nik@postgres.ai>
2019-08-16 12:06:44 +0000

    436-dmius-F005-indexes-size-fix artifacts

--- 436-dmius-F005-indexes-size-fix/json_reports/1_2019_08_16T11_30_39_+0000/K_query_groups/42_3.sql
+++
@@ -1,4 +0,0 @@
--- queryid: 9128827601981150000
--- NOTICE: the first 50k characters
--- NOTICE: current query size (bytes): '45'
-create index concurrently i_u_106 on t_106(i)
---
+++ 436-dmius-F005-indexes-size-fix/json_reports/1_2019_08_16T12_04_58_+0000/K_query_groups/71_3.sql
@@ -0,0 +1,4 @@
+-- queryid: 3666704987953131000
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '41'
+create index concurrently i_u_8 on t_8(i)
