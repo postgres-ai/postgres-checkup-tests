b98906465a381777a473d40475ac34e05490aab8
postgres-ai <nik@postgres.ai>
2019-08-16 16:27:35 +0000

    master artifacts

---
+++ master/json_reports/1_2019_08_16T16_25_51_+0000/K_query_groups/13_3.sql
@@ -0,0 +1,4 @@
+-- queryid: -4922882474069691000
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '14'
+VACUUM ANALYZE
---
+++ master/json_reports/1_2019_08_16T16_25_51_+0000/K_query_groups/50_3.sql
@@ -0,0 +1,4 @@
+-- queryid: -6290564462188021000
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '23'
+select pg_reload_conf()
