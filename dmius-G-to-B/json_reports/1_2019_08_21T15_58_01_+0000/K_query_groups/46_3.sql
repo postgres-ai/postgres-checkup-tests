4352793c4639b9cec24d51f5c7877a76526ebc6e
postgres-ai <nik@postgres.ai>
2019-08-21 15:59:47 +0000

    dmius-G-to-B artifacts

---
+++ dmius-G-to-B/json_reports/1_2019_08_21T15_58_01_+0000/K_query_groups/46_3.sql
@@ -0,0 +1,4 @@
+-- queryid: -7862275411983427000
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '77'
+create index concurrently t_with_redundant_idx_f1 on t_with_redundant_idx(f1)
