29e9077177850cb86e9c17d92cb83610d88bbd32
postgres-ai <nik@postgres.ai>
2019-08-22 16:52:21 +0000

    426-dmius-send-report-data artifacts

--- 426-dmius-send-report-data/json_reports/1_2019_08_22T15_57_21_+0000/K_query_groups/51_3.sql
+++
@@ -1,4 +0,0 @@
--- queryid: 1633515346756009000
--- NOTICE: the first 50k characters
--- NOTICE: current query size (bytes): '41'
-create index concurrently i_79 on t_79(i)
--- 426-dmius-send-report-data/json_reports/1_2019_08_22T15_57_21_+0000/K_query_groups/65_3.sql
+++
@@ -1,4 +0,0 @@
--- queryid: -949153349382420900
--- NOTICE: the first 50k characters
--- NOTICE: current query size (bytes): '41'
-create index concurrently i_76 on t_76(i)
