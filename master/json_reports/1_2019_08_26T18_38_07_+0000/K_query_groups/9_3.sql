179f5f05df099e797d0d5d7e83b48c0c22b57678
postgres-ai <nik@postgres.ai>
2019-08-26 18:40:06 +0000

    master artifacts

---
+++ master/json_reports/1_2019_08_26T18_38_07_+0000/K_query_groups/9_3.sql
@@ -0,0 +1,4 @@
+-- queryid: 39141847517093180
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '90'
+create index t_with_redundant_ref_idx_3 on public.t_with_redundant_ref_idx using btree (i)
