2501a360859fb791b9feb26202b0ea86c37bd716
postgres-ai <nik@postgres.ai>
2019-08-30 07:49:44 +0000

    dmius-f004-fix artifacts

---
+++ dmius-f004-fix/json_reports/1_2019_08_30T07_47_47_+0000/K_query_groups/35_3.sql
@@ -0,0 +1,4 @@
+-- queryid: -7990293000632035000
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '79'
+create index concurrently t_with_redundant_idx_f3_1 on t_with_redundant_idx(f3)
