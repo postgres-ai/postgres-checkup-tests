3617c03b45eb050878405c08a89132ea6e6a0e61
postgres-ai <nik@postgres.ai>
2019-09-04 18:40:28 +0000

    426-dmius-send-report-data artifacts

--- 426-dmius-send-report-data/json_reports/1_2019_09_03T17_20_59_+0000/K_query_groups/86_3.sql
+++
@@ -1,4 +0,0 @@
--- queryid: -383133346259098400
--- NOTICE: the first 50k characters
--- NOTICE: current query size (bytes): '64'
-create table t_14 as select i from generate_series(1, 1000) _(i)
---
+++ 426-dmius-send-report-data/json_reports/1_2019_09_04T18_38_55_+0000/K_query_groups/36_3.sql
@@ -0,0 +1,4 @@
+-- queryid: -5310216488601905000
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '43'
+create index concurrently i_u_95 on t_95(i)
