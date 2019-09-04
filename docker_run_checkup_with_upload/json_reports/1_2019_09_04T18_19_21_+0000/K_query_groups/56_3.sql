57fdf94f7293bbe559401bf28b53a0bc3b7fce73
postgres-ai <nik@postgres.ai>
2019-09-04 18:20:54 +0000

    docker_run_checkup_with_upload artifacts

---
+++ docker_run_checkup_with_upload/json_reports/1_2019_09_04T18_19_21_+0000/K_query_groups/56_3.sql
@@ -0,0 +1,4 @@
+-- queryid: -5447541027672477000
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '43'
+create index concurrently i_r_82 on t_82(i)
---
+++ docker_run_checkup_with_upload/json_reports/1_2019_09_04T18_19_21_+0000/K_query_groups/78_3.sql
@@ -0,0 +1,4 @@
+-- queryid: -3717551161508284400
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '41'
+create index concurrently i_r_1 on t_1(i)
