a09798eb87b8a473f1a45feca7e9c07ae1670f80
postgres-ai <nik@postgres.ai>
2019-09-16 05:10:59 +0000

    440-dmius-connn-timeout artifacts

--- 440-dmius-connn-timeout/json_reports/1_2019_09_11T12_51_17_+0000/K_query_groups/11_3.sql
+++
@@ -1,4 +0,0 @@
--- queryid: 2579118609572620000
--- NOTICE: the first 50k characters
--- NOTICE: current query size (bytes): '43'
-create index concurrently i_r_26 on t_26(i)
--- 440-dmius-connn-timeout/json_reports/1_2019_09_11T12_51_17_+0000/K_query_groups/67_3.sql
+++
@@ -1,4 +0,0 @@
--- queryid: 2716981810434346500
--- NOTICE: the first 50k characters
--- NOTICE: current query size (bytes): '43'
-create index concurrently i_r_35 on t_35(i)
