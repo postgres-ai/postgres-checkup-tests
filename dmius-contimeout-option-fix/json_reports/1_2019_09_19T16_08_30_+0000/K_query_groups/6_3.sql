4031460846976187047aca91a242d96d1a92baf7
postgres-ai <nik@postgres.ai>
2019-09-19 16:10:08 +0000

    dmius-contimeout-option-fix artifacts

---
+++ dmius-contimeout-option-fix/json_reports/1_2019_09_19T16_08_30_+0000/K_query_groups/6_3.sql
@@ -0,0 +1,4 @@
+-- queryid: 8824971584930246000
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '43'
+create index concurrently i_r_67 on t_67(i)
---
+++ dmius-contimeout-option-fix/json_reports/1_2019_09_19T16_08_30_+0000/K_query_groups/80_3.sql
@@ -0,0 +1,4 @@
+-- queryid: 3856153419129616000
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '43'
+create index concurrently i_u_68 on t_68(i)
