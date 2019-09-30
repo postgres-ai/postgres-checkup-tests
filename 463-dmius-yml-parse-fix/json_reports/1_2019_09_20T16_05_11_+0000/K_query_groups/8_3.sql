f2d028ffafdfa088dad22bdeca12792052fe0fdb
postgres-ai <nik@postgres.ai>
2019-09-30 07:28:32 +0000

    463-dmius-yml-parse-fix artifacts

--- 463-dmius-yml-parse-fix/json_reports/1_2019_09_20T16_05_11_+0000/K_query_groups/8_3.sql
+++
@@ -1,4 +0,0 @@
--- queryid: -395470359289702340
--- NOTICE: the first 50k characters
--- NOTICE: current query size (bytes): '43'
-create index concurrently i_u_78 on t_78(i)
---
+++ 463-dmius-yml-parse-fix/json_reports/1_2019_09_30T07_27_00_+0000/K_query_groups/55_3.sql
@@ -0,0 +1,4 @@
+-- queryid: -185707737803468000
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '43'
+create index concurrently i_r_68 on t_68(i)
