d85af2773c6d3ab7abd22f64be31c1ebad4be7de
postgres-ai <nik@postgres.ai>
2019-10-04 13:41:42 +0000

    dmius-h002-redundant-fix artifacts

--- dmius-h002-redundant-fix/json_reports/1_2019_10_04T13_19_58_+0000/K_query_groups/16_3.sql
+++
@@ -1,4 +0,0 @@
--- queryid: -5371677106063814000
--- NOTICE: the first 50k characters
--- NOTICE: current query size (bytes): '43'
-create index concurrently i_u_48 on t_48(i)
--- dmius-h002-redundant-fix/json_reports/1_2019_10_04T13_19_58_+0000/K_query_groups/87_3.sql
+++
@@ -1,4 +0,0 @@
--- queryid: -2188472341511549200
--- NOTICE: the first 50k characters
--- NOTICE: current query size (bytes): '41'
-create index concurrently i_r_7 on t_7(i)
