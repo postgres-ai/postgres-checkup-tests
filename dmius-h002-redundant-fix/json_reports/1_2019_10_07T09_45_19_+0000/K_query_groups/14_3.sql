1435e864300a3e2c130496b8dcc84b3cea8475c8
postgres-ai <nik@postgres.ai>
2019-10-08 11:25:13 +0000

    dmius-h002-redundant-fix artifacts

--- dmius-h002-redundant-fix/json_reports/1_2019_10_07T09_45_19_+0000/K_query_groups/14_3.sql
+++
@@ -1,4 +0,0 @@
--- queryid: 6233306292145300000
--- NOTICE: the first 50k characters
--- NOTICE: current query size (bytes): '43'
-create index concurrently i_r_24 on t_24(i)
---
+++ dmius-h002-redundant-fix/json_reports/1_2019_10_08T11_23_27_+0000/K_query_groups/83_3.sql
@@ -0,0 +1,4 @@
+-- queryid: 6700951604354476000
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '43'
+create index concurrently i_r_22 on t_22(i)
