ba8080360c8b994a4f19000e978292e17fa0f45f
postgres-ai <nik@postgres.ai>
2019-10-11 05:34:06 +0000

    master artifacts

---
+++ master/json_reports/1_2019_10_11T05_32_33_+0000/K_query_groups/53_3.sql
@@ -0,0 +1,4 @@
+-- queryid: -3498952347021279
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '75'
+create table t_f004_empty as select i from generate_series(1, 1000000) _(i)
