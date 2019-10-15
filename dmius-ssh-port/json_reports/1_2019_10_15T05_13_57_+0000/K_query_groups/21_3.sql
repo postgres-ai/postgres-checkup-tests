729ec527be6a7fa1dea0754cfa9f6c5882bf8352
postgres-ai <nik@postgres.ai>
2019-10-15 15:52:35 +0000

    dmius-ssh-port artifacts

--- dmius-ssh-port/json_reports/1_2019_10_15T05_13_57_+0000/K_query_groups/21_3.sql
+++
@@ -1,4 +0,0 @@
--- queryid: -3084515071399118300
--- NOTICE: the first 50k characters
--- NOTICE: current query size (bytes): '43'
-create index concurrently i_r_41 on t_41(i)
--- dmius-ssh-port/json_reports/1_2019_10_15T05_13_57_+0000/K_query_groups/53_3.sql
+++
@@ -1,4 +0,0 @@
--- queryid: -185707737803468000
--- NOTICE: the first 50k characters
--- NOTICE: current query size (bytes): '43'
-create index concurrently i_r_68 on t_68(i)
