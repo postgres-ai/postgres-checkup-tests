eb3d0805fcbb5096b898378c77de6763398aff8e
postgres-ai <nik@postgres.ai>
2019-10-15 15:44:11 +0000

    dmius-bloat-recomendation-improve artifacts

---
+++ dmius-bloat-recomendation-improve/json_reports/1_2019_10_15T15_42_31_+0000/K_query_groups/32_3.sql
@@ -0,0 +1,4 @@
+-- queryid: -4398627158312638000
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '29'
+update t_with_bloat set i = i
---
+++ dmius-bloat-recomendation-improve/json_reports/1_2019_10_15T15_42_31_+0000/K_query_groups/4_3.sql
@@ -0,0 +1,4 @@
+-- queryid: 4140264369554246700
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '49'
+create index ctnr_idx01 on ctnr using btree(pnum)
