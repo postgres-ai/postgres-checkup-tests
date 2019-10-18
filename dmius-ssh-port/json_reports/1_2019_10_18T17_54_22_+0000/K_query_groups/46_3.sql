4c8b851b4c1814759772297515b58b1958f3778e
postgres-ai <nik@postgres.ai>
2019-10-18 17:58:46 +0000

    dmius-ssh-port artifacts

--- dmius-ssh-port/json_reports/1_2019_10_18T17_54_22_+0000/K_query_groups/46_3.sql
+++
@@ -1,4 +0,0 @@
--- queryid: 4093679934485142000
--- NOTICE: the first 50k characters
--- NOTICE: current query size (bytes): '43'
-create index concurrently i_u_80 on t_80(i)
---
+++ dmius-ssh-port/json_reports/1_2019_10_18T17_57_07_+0000/K_query_groups/44_3.sql
@@ -0,0 +1,4 @@
+-- queryid: 7535613378973123000
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '64'
+create table t_24 as select i from generate_series(1, 1000) _(i)
