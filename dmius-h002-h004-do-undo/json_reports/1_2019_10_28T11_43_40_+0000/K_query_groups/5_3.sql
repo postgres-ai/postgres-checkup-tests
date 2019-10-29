591df24875b83a826f28b60dfd97a8c1f5e31713
postgres-ai <nik@postgres.ai>
2019-10-29 06:14:10 +0000

    dmius-h002-h004-do-undo artifacts

--- dmius-h002-h004-do-undo/json_reports/1_2019_10_28T11_43_40_+0000/K_query_groups/5_3.sql
+++
@@ -1,4 +0,0 @@
--- queryid: 6383488641420515000
--- NOTICE: the first 50k characters
--- NOTICE: current query size (bytes): '43'
-create index concurrently i_r_19 on t_19(i)
---
+++ dmius-h002-h004-do-undo/json_reports/1_2019_10_29T06_12_34_+0000/K_query_groups/1_3.sql
@@ -0,0 +1,4 @@
+-- queryid: -4387101759094203000
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '45'
+create index concurrently i_u_105 on t_105(i)
