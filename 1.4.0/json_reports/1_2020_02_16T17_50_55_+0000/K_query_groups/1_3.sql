1d3d3badc3d61fd0835429d2df39330e30d8e1ff
postgres-ai <nik@postgres.ai>
2020-02-16 17:52:46 +0000

    1.4.0 artifacts

---
+++ 1.4.0/json_reports/1_2020_02_16T17_50_55_+0000/K_query_groups/1_3.sql
@@ -0,0 +1,4 @@
+-- queryid: 709055140052547300
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '39'
+select from pg_stat_statements limit $1
---
+++ 1.4.0/json_reports/1_2020_02_16T17_50_55_+0000/K_query_groups/38_3.sql
@@ -0,0 +1,4 @@
+-- queryid: 4227063200314075600
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '94'
+create table public.t_with_redundant_ref_idx as select i from generate_series(1, 1000000) _(i)
