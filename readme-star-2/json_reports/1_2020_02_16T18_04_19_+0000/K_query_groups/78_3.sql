7c6534a2d01e0dbec5552e0fb063b719e43ea09e
postgres-ai <nik@postgres.ai>
2020-02-16 18:06:03 +0000

    readme-star-2 artifacts

---
+++ readme-star-2/json_reports/1_2020_02_16T18_04_19_+0000/K_query_groups/78_3.sql
@@ -0,0 +1,4 @@
+-- queryid: 8761780441290022000
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '24'
+delete from t_f004_empty
---
+++ readme-star-2/json_reports/1_2020_02_16T18_04_19_+0000/K_query_groups/82_3.sql
@@ -0,0 +1,4 @@
+-- queryid: 5035677124990811000
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '43'
+create index concurrently i_r_71 on t_71(i)
