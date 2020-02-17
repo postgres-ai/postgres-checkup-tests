d9c7b3d937e3e2d04848b95e93f8c0bd143561fc
postgres-ai <nik@postgres.ai>
2020-02-17 00:10:11 +0000

    master artifacts

---
+++ master/json_reports/1_2020_02_17T00_08_24_+0000/K_query_groups/65_3.sql
@@ -0,0 +1,4 @@
+-- queryid: -5601151487238124000
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '81'
+create table t_red_fk_1 as select id::int8 from generate_series(0, 1000000) _(id)
---
+++ master/json_reports/1_2020_02_17T00_08_24_+0000/K_query_groups/84_3.sql
@@ -0,0 +1,4 @@
+-- queryid: -7264213258165851000
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '63'
+create table t_1 as select i from generate_series(1, 1000) _(i)
