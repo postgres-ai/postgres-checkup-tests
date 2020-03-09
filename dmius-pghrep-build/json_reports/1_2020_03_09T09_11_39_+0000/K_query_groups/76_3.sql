8e48582cabc17238c5f1929ba7297fb43bd3ef8c
postgres-ai <nik@postgres.ai>
2020-03-09 09:13:05 +0000

    dmius-pghrep-build artifacts

---
+++ dmius-pghrep-build/json_reports/1_2020_03_09T09_11_39_+0000/K_query_groups/76_3.sql
@@ -0,0 +1,4 @@
+-- queryid: -4942022625511173000
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '46'
+create index ctnr_idx03 on ctnr using btree(b)
---
+++ dmius-pghrep-build/json_reports/1_2020_03_09T09_11_39_+0000/K_query_groups/9_3.sql
@@ -0,0 +1,4 @@
+-- queryid: 7246179927735003000
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '43'
+create index concurrently i_u_60 on t_60(i)
