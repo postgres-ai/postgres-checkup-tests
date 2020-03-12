8825af89fe3baa9bf472826f19704648875e05e6
postgres-ai <nik@postgres.ai>
2020-03-12 10:23:44 +0000

    dmius-k00-typo-fix artifacts

---
+++ dmius-k00-typo-fix/json_reports/1_2020_03_12T10_22_12_+0000/K_query_groups/3_2.sql
@@ -0,0 +1,4 @@
+-- queryid: -7538006300762366000
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '39'
+select from pg_stat_statements limit $1
---
+++ dmius-k00-typo-fix/json_reports/1_2020_03_12T10_22_12_+0000/K_query_groups/49_3.sql
@@ -0,0 +1,4 @@
+-- queryid: 2858007219600663000
+-- NOTICE: the first 50k characters
+-- NOTICE: current query size (bytes): '43'
+create index concurrently i_u_53 on t_53(i)
